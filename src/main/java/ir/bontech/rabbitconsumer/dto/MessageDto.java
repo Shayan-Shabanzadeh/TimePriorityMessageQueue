66e3b5850f4ed772546ad6da2cb6f32e7bb183be
Shayan <shayanshabanzadeh0@gmail.com>
2023-09-05 17:44:24 +0430

    reformat: clean up code in MessageDto.java

--- src/main/java/ir/bontech/rabbitconsumer/dto/MessageDto.java
+++ src/main/java/ir/bontech/rabbitconsumer/dto/MessageDto.java
@@ -18,10 +18,7 @@ public class MessageDto implements Comparable<MessageDto> {
     private long creationTimestamp;
     private int priority;
 
-    // Weights for priority and timestamp
-//    private static final double PRIORITY_WEIGHT = 0.7;
-//    private static final double TIMESTAMP_WEIGHT = 0.3;
-    private static final long TIME_WINDOW = 2 * 60 * 1000; // 2 minutes in milliseconds
+    private static final long TIME_WINDOW = 60 * 1000; // 2 minutes in milliseconds
 
 
     @Override
@@ -36,34 +33,33 @@ public class MessageDto implements Comparable<MessageDto> {
                 ", content='" + content + '\'' +
                 ", creationTimestamp=" + formatter.format(creationTime) +
                 ", priority=" + priority +
-                ", calculated priority=" + calculateCompositeScore(this , currentTime)+
+                ", calculated priority=" + calculateCompositeScore(this, currentTime) +
                 '}';
     }
 
 
     @Override
     public int compareTo(MessageDto other) {
-        // Calculate composite scores
         Instant instant = Instant.now();
         long timeStampMillis = instant.toEpochMilli();
 
-
+        // Calculate composite scores
         double thisCompositeScore = calculateCompositeScore(this, timeStampMillis);
         double otherCompositeScore = calculateCompositeScore(other, timeStampMillis);
 
         // Compare based on composite scores
-        int isBiggerByPriority =  Double.compare(otherCompositeScore, thisCompositeScore); // Higher score first
-        if (isBiggerByPriority == 0 ) {
+        int isBiggerByPriority = Double.compare(otherCompositeScore, thisCompositeScore); // Higher score first
+        if (isBiggerByPriority == 0) {
             int isBiggerByCreationTime = Double.compare(this.creationTimestamp, other.creationTimestamp);
-            if (isBiggerByCreationTime == 0 ){
-                return Integer.compare(this.priority ,other.priority);
+            if (isBiggerByCreationTime == 0) {
+                return Integer.compare(this.priority, other.priority);
             }
             return isBiggerByCreationTime;
         }
         return isBiggerByPriority;
     }
 
-    private double calculateCompositeScore(MessageDto message, long currentTime) {
+    private int calculateCompositeScore(MessageDto message, long currentTime) {
         int additional_priority = (int) ((currentTime - message.creationTimestamp) / TIME_WINDOW);
         return additional_priority + message.getPriority();
     }
