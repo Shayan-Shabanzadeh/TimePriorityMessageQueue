7208cd0b3e49e70e8d9d205f8cb50c5475cb1e38
Shayan <shayanshabanzadeh0@gmail.com>
2023-09-05 17:53:15 +0430

    reformat: clean code in RabbitConsumerApplication add: add some dependencies to pom.xml

--- src/main/java/ir/bontech/rabbitconsumer/RabbitConsumerApplication.java
+++ src/main/java/ir/bontech/rabbitconsumer/RabbitConsumerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitConsumerApplication.class, args);
+    }
 
 }
