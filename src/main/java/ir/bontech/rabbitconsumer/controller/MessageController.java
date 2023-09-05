6a93db0aeb38b78176d63f2533d70d3d4bba03d3
Shayan <shayanshabanzadeh0@gmail.com>
2023-09-05 21:05:11 +0430

    refactor: add logger to rabbitController

--- src/main/java/ir/bontech/rabbitconsumer/controller/MessageController.java
+++ src/main/java/ir/bontech/rabbitconsumer/controller/MessageController.java
@@ -2,6 +2,8 @@ package ir.bontech.rabbitconsumer.controller;
 
 import ir.bontech.rabbitconsumer.dto.MessageDto;
 import ir.bontech.rabbitconsumer.service.MessagePublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
     private final MessagePublisher messagePublisher;
 
@@ -27,8 +31,10 @@ public class MessageController {
             messagePublisher.sendMessage(message);
             return ResponseEntity.ok("Message published successfully");
         } catch (Exception e) {
-            String errorMessage = "Error publishing message: " + e.getMessage();
-            e.printStackTrace();
+            // Handle unexpected exceptions
+            String errorMessage = "An unexpected error occurred while publishing the message.";
+            // Log the error
+            logger.error(errorMessage, e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
         }
     }
