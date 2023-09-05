2b3d498c6f7d22413d6fad5bb48d5253e8a233eb
Shayan <shayanshabanzadeh0@gmail.com>
2023-09-05 17:34:26 +0430

    refactor: add threadpoolSize , add logger , add comments

--- src/main/java/ir/bontech/rabbitconsumer/service/MessageConsumer.java
+++ src/main/java/ir/bontech/rabbitconsumer/service/MessageConsumer.java
@@ -2,38 +2,56 @@ package ir.bontech.rabbitconsumer.service;
 
 import ir.bontech.rabbitconsumer.dto.MessageDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
 
 @Service
 @Getter
+@Primary
+@NoArgsConstructor
 public class MessageConsumer {
 
-    @Value("${spring.rabbitmq.template.default-receive-queue}")
-    private String queueName;
+    @Value("${consumer.thread.pool.size:1}")
+    private int threadPoolSize;
+    protected final PriorityBlockingQueue<MessageDto> messageQueue = new PriorityBlockingQueue<>();
+    private ExecutorService executorService;
 
-    private final PriorityBlockingQueue<MessageDto> messageQueue = new PriorityBlockingQueue<>();
-    private final ExecutorService executorService = Executors.newFixedThreadPool(1); // Number of consumer threads
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
+
+    public MessageConsumer(int threadPoolSize){
+        this.threadPoolSize = threadPoolSize;
+    }
+
+
+    //Method to start startConsumingMessage to process messages
     @PostConstruct
     public void init() {
+        logger.info("Consumer thread pool started with {} threads.", threadPoolSize);
+        executorService = Executors.newFixedThreadPool(threadPoolSize); // Create ExecutorService after properties are read
         startConsumingMessages();
     }
 
+    //Method to consume message from Rabbit
     public void handleMessage(MessageDto message) {
-        // Assuming you want to prioritize based on the 'priority' field of MessageDto
-        System.out.println("Consume message: " + message);
+        logger.info("Consume message: {}", message);
+
         messageQueue.add(message);
     }
 
 
-
-
-    // Method to start consuming and processing messages
+    // Method to start consuming message from Priority queue and process them
     public void startConsumingMessages() {
         executorService.execute(() -> {
             while (true) {
@@ -55,12 +73,15 @@ public class MessageConsumer {
         });
     }
 
-    void processMessages(List<MessageDto> messages) {
+
+
+    //Method for processing messages
+    protected void processMessages(List<MessageDto> messages) throws InterruptedException {
         // Implement your batch message processing logic here
         for (MessageDto message : messages) {
-            System.out.println("Processing message: " + message);
+            logger.info("Processing message: {}", message);
         }
-        System.out.println("-------------------------------");
+        logger.info("-------------------------------");
     }
 
     // Method to gracefully shut down the consumer
