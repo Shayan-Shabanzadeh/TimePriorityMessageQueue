4e6b3ca01fcd9837298b9418b9e60bc0d386226d
Shayan <shayanshabanzadeh0@gmail.com>
2023-09-05 17:43:29 +0430

    refactor: remove executorService , processedMessages and tearDown in MessageConsumerTest.java refactor: change MessageConsumer() to MessageConsumer(#threadNumber)

--- src/test/java/ir/bontech/rabbitconsumer/service/MessageConsumerTest.java
+++ src/test/java/ir/bontech/rabbitconsumer/service/MessageConsumerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -16,21 +17,12 @@ import static org.mockito.Mockito.*;
 @ActiveProfiles("test")
 class MessageConsumerTest {
 
-    private ExecutorService executorService;
     private MessageConsumer messageConsumer;
-    private List<MessageDto> processedMessages;
 
     @BeforeEach
     void setUp() {
-        messageConsumer = new MessageConsumer(); // Create an instance of the actual class
+        messageConsumer = new MessageConsumer(5); // Create an instance of the actual class
         messageConsumer.init(); // Initialize the messageConsumer
-    }
-
-    @AfterEach
-    void tearDown() {
-        if (executorService != null) {
-            executorService.shutdownNow();
-        }
     }
 
     @Test
@@ -59,8 +51,9 @@ class MessageConsumerTest {
 
     @Test
     void testMessageProcessingBasedOnPriority() throws InterruptedException {
+        var currentTime = Instant.now().toEpochMilli();
         // Create a spy of the actual MessageConsumer instance
-        MessageConsumer messageConsumer = spy(new MessageConsumer());
+        MessageConsumer messageConsumer = spy(new MessageConsumer(1));
 
         // Mock the PriorityBlockingQueue
         PriorityBlockingQueue<MessageDto> messageQueueMock = mock(PriorityBlockingQueue.class);
@@ -70,18 +63,15 @@ class MessageConsumerTest {
         messageConsumer.init();
 
         // Simulate sending mock messages with varying priorities
-        MessageDto message1 = new MessageDto("1", "content1", 1693333777893L, 3);
-        MessageDto message2 = new MessageDto("2", "content2", 1693333777893L, 2);
-        MessageDto message3 = new MessageDto("3", "content3", 1693333777893L, 4);
+        MessageDto message1 = new MessageDto("1", "content1", currentTime, 3);
+        MessageDto message2 = new MessageDto("2", "content2", currentTime, 2);
+        MessageDto message3 = new MessageDto("3", "content3", currentTime, 4);
 
 
         // Simulate calling handleMessage for the actual instance
         messageConsumer.handleMessage(message1);
         messageConsumer.handleMessage(message2);
         messageConsumer.handleMessage(message3);
-//        messageConsumer.handleMessage(message4);
-//        messageConsumer.handleMessage(message5);
-//        messageConsumer.handleMessage(message6);
 
         // Wait for processing to complete
         TimeUnit.SECONDS.sleep(5);
@@ -89,12 +79,6 @@ class MessageConsumerTest {
         // Verify the interactions
         verify(messageConsumer, times(1)).init();
         verify(messageConsumer, times(1)).startConsumingMessages();
-
-        // Verify the order of processed messages
-//        InOrder inOrder = inOrder(messageQueueMock);
-//        inOrder.verify(messageQueueMock).add(message1);
-//        inOrder.verify(messageQueueMock).add(message2);
-//        inOrder.verify(messageQueueMock).add(message3);
 
         // Verify that processMessages was invoked with the captured messages
         ArgumentCaptor<List<MessageDto>> argumentCaptor = ArgumentCaptor.forClass(List.class);
